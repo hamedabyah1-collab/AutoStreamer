ac05c60610010896c24ba5c26673a3d66cc058ad
hamedabyah1-collab <hamed.abyah1@gmail.com>
2025-12-19 08:30:03 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,57 +1,51 @@
 plugins {
-alias(libs.plugins.androidApplication)
-alias(libs.plugins.kotlinAndroid)
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+apply(plugin = "com.android.application")
+apply(plugin = "org.jetbrains.kotlin.android")
 
 android {
-namespace = "com.threebya7.carbrowser"
-compileSdk = 34
+    namespace = "com.aidetector.app"
+    compileSdk = 34
 
-defaultConfig {  
-    applicationId = "com.threebya7.carbrowser"  
-    minSdk = 28  
-    targetSdk = 34  
-    versionCode = 1  
-    versionName = "1.0"  
+    defaultConfig {
+        applicationId = "com.aidetector.app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"  
-}  
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 
-buildTypes {  
-    release {  
-        isMinifyEnabled = false  
-        proguardFiles(  
-            getDefaultProguardFile("proguard-android-optimize.txt"),  
-            "proguard-rules.pro"  
-        )  
-    }  
-}  
-compileOptions {  
-    sourceCompatibility = JavaVersion.VERSION_1_8  
-    targetCompatibility = JavaVersion.VERSION_1_8  
-}  
-kotlinOptions {  
-    jvmTarget = "1.8"  
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
+repositories {
+    google()
+    mavenCentral()
 }
 
 dependencies {
-// Core Android
-implementation(libs.androidx.core.ktx)
-implementation(libs.androidx.appcompat)
-implementation(libs.material)
-
-// Android Auto Car App Library - Essential for car screen  
-implementation("androidx.car.app:app:1.4.0")  
-
-// WebView - To display web content  
-implementation("androidx.webkit:webkit:1.9.0")  
-
-// Testing  
-testImplementation(libs.junit)  
-androidTestImplementation(libs.androidx.junit)  
-androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.3.1")
 }
-
